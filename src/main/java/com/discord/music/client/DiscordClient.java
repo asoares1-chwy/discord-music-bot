088cd6a13d5ec45e8375c124a10ead3aafc4c8df
Adriano Soares <asoares1@chewy.com>
2023-02-17 13:18:49 -0500

    remove unused imports

--- src/main/java/com/discord/music/client/DiscordClient.java
+++ src/main/java/com/discord/music/client/DiscordClient.java
@@ -6,7 +6,6 @@ import com.discord.music.model.ApplicationCommand;
 import com.discord.music.model.ApplicationCommandRequest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.*;
 import org.springframework.http.HttpMethod;
