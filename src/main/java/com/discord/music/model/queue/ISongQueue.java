3185618d13586c62741049b4168063d274a8daf1
Adriano Soares <asoares1@chewy.com>
2023-03-02 19:14:40 -0500

    documentation

--- src/main/java/com/discord/music/model/queue/ISongQueue.java
+++ src/main/java/com/discord/music/model/queue/ISongQueue.java
@@ -2,9 +2,29 @@ package com.discord.music.model.queue;
 
 import com.discord.music.model.YouTubeURI;
 
+/**
+ * Defines a set of operations for a song queue.
+ */
 public interface ISongQueue {
+    /**
+     * Appends a song to the end of the queue.
+     * @param uri the YouTube resource to play.
+     */
     void addSong(YouTubeURI uri);
+
+    /**
+     * Returns the song at the head of the queue. Does not modify the queue.
+     * @return the song at the head of the queue.
+     */
     YouTubeURI currentlyPlaying();
+
+    /**
+     * Removes the song at the head of the queue. An event is published to inform the player.
+     */
     void skipSong();
+
+    /**
+     * Removes all elements from the queue. An event is published to inform the player.
+     */
     void clearQueue();
 }
