b4ce7a3f96302d60a6e2b18ac4348fc5939052a8
unknown <asoares1@chewy.com>
2023-03-04 21:47:14 -0500

    logging

--- src/main/java/com/discord/music/service/CommandInstallationService.java
+++ src/main/java/com/discord/music/service/CommandInstallationService.java
@@ -4,6 +4,7 @@ import com.discord.music.config.properties.PublicBotProperties;
 import com.discord.music.model.MusicBotCommand;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.discordjson.json.ApplicationCommandData;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -13,12 +14,14 @@ import java.util.stream.Collectors;
 
 @Service
 public class CommandInstallationService {
+    private final Logger logger;
     private final GatewayDiscordClient discordClient;
     private final PublicBotProperties pbp;
 
-    public CommandInstallationService(GatewayDiscordClient discordClient, PublicBotProperties pbp) {
+    public CommandInstallationService(Logger logger, GatewayDiscordClient discordClient, PublicBotProperties pbp) {
         this.discordClient = discordClient;
         this.pbp = pbp;
+        this.logger = logger;
     }
 
     private Set<MusicBotCommand> getMissingCommands() {
@@ -32,7 +35,11 @@ public class CommandInstallationService {
             throw new RuntimeException("unable to verify guild commands of guild " + pbp.getGuildId());
         }
         return Arrays.stream(MusicBotCommand.values())
-                .filter(cmd -> sm.contains(cmd.getLiteralCommand()))
+                .filter(cmd -> {
+                    logger.info("command '{}' already installed on server. no action will be taken.",
+                            cmd.getLiteralCommand());
+                    return sm.contains(cmd.getLiteralCommand());
+                })
                 .collect(Collectors.toSet());
     }
 
