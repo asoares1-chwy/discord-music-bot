65fc52ae0d813b0e1338249aa67b8b71c5cc00a7
unknown <asoares1@chewy.com>
2023-03-04 23:21:40 -0500

    pattern matcher doesn't suck ass now

--- src/main/java/com/discord/music/model/YouTubeURI.java
+++ src/main/java/com/discord/music/model/YouTubeURI.java
@@ -1,22 +1,24 @@
 package com.discord.music.model;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class YouTubeURI {
     private final String uri;
 
-    private final static Pattern pattern = Pattern
-            .compile("https://www.youtube.com/watch\\?v=[A-Za-z0-9]+", Pattern.CASE_INSENSITIVE);
+    private final static Pattern BASE_YOUTUBE_URI_PATTERN = Pattern
+            .compile("^(?:https://)?www.youtube.com/watch\\?v=[A-Za-z0-9_-]+", Pattern.CASE_INSENSITIVE);
 
     private YouTubeURI(String uri) {
         this.uri = uri;
     }
 
     public static YouTubeURI fromUriString(String uri) {
-        if (!pattern.matcher(uri).matches()) {
+        Matcher baseUriMatcher = BASE_YOUTUBE_URI_PATTERN.matcher(uri);
+        if (!baseUriMatcher.find()) {
             throw new IllegalArgumentException("uri does not match expected pattern of YouTube link.");
         }
-        return new YouTubeURI(uri);
+        return new YouTubeURI(baseUriMatcher.group());
     }
 
     public String getUri() {
