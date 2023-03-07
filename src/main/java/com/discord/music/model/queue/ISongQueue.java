92bc8cd5dfc947b5d3751c9d010e2547981c2f16
Adriano Soares <asoares1@chewy.com>
2023-03-07 12:08:59 -0500

    documentation

--- src/main/java/com/discord/music/model/queue/ISongQueue.java
+++ src/main/java/com/discord/music/model/queue/ISongQueue.java
@@ -5,12 +5,12 @@ import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import java.util.List;
 
 /**
- * Defines a set of operations for a song queue.
+ * Defines a set of operations for a Song Queue.
  */
 public interface ISongQueue {
     /**
-     * Appends a song to the end of the queue.
-     * @param uri the YouTube resource to play.
+     * Appends a song to the end of the queue. If the queue is empty, the AudioTrack will play immediately.
+     * @param uri the AudioTrack resource to play.
      */
     void addSong(AudioTrack uri);
 
@@ -21,20 +21,22 @@ public interface ISongQueue {
     AudioTrack currentlyPlaying();
 
     /**
-     * Removes the song at the head of the queue. An event is published to inform the player.
+     * Cancels the currently playing track. If the queue contains more elements, the next track will begin playing.
      * @return true if the song was skipped, and false otherwise.
      */
     boolean skipSong();
 
     /**
-     * Removes all elements from the queue. An event is published to inform the player.
+     * Removes all elements from the queue. Cancels the currently playing AudioTrack.
+     * The queue will always be empty after this operation.
      * @return true if the queue was cleared, or if a track was stopped; otherwise false.
      */
     boolean clearQueue();
 
     /**
-     * Returns a list view of the remaining songs in the queue. Does not return the currently playing track.
-     * @return A List containing an ordered list of songs in the queue.
+     * Returns an immutable list view of the remaining songs in the queue.
+     * Does not return the currently playing track.
+     * @return An ordered list of songs in the queue.
      */
     List<AudioTrack> peekQueueContents();
 
