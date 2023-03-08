3e8375c39083f025b7fff7f6b757c8b743cb8cbd
unknown <asoares1@chewy.com>
2023-03-07 23:35:07 -0500

    silly skip bug

--- src/main/java/com/discord/music/component/handlers/SkipCommand.java
+++ src/main/java/com/discord/music/component/handlers/SkipCommand.java
@@ -3,6 +3,7 @@ package com.discord.music.component.handlers;
 import com.discord.music.component.audio.SongQueue;
 import com.discord.music.model.ISongQueue;
 import com.discord.music.model.CommandHandler;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
@@ -17,9 +18,9 @@ public class SkipCommand implements CommandHandler<ChatInputInteractionEvent> {
 
     @Override
     public Mono<Void> executeOnCommand(ChatInputInteractionEvent event) {
+        AudioTrack currentTrack = songQueue.currentlyPlaying();
         if (songQueue.skipSong()) {
-            String currentTrack = songQueue.currentlyPlaying().getInfo().title;
-            return event.reply("Skipped <" + currentTrack + ">");
+            return event.reply("Skipped <" + currentTrack.getInfo().title + ">");
         }
         return event
                 .reply("No song is playing, and the queue is empty.")
